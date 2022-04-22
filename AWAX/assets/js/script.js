66bcb2d86bff99df8e805e03b917082b87c43fc0
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-04-22 11:17:36 -0300

    Scroll top button and menu mobile included.

--- AWAX/assets/js/script.js
+++ AWAX/assets/js/script.js
@@ -5,5 +5,39 @@ function infoToggle(e) {
         div.classList.remove('showInfo');
     } else {
         div.classList.add('showInfo');
+    };
+};
+
+function scrollToggle() {
+    const element = document.querySelector('#scrollTopButton');
+    if (window.scrollY == 0) {
+        element.style.display = 'none';
+    } else {
+        element.style.display = 'flex';
     }
 }
+
+window.addEventListener('scroll', scrollToggle);
+
+function backToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+};
+
+function menuToggle() {
+    let menu = {
+        show: "839px",
+        hidden: "0px"
+    }
+    let element = document.getElementById('menuMobile');
+
+    if (element.style.height == "839px") {
+        element.classList.remove('paddingMenuMobile');
+        element.style.height = menu.hidden;
+    } else {
+        element.classList.add('paddingMenuMobile');
+        element.style.height = menu.show;
+    };
+};
