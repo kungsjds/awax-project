c49b6a24116d5cbfab0e239c2dd568e3838d91dc
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-05-10 22:31:15 -0300

    Action completed on sliders. Project finished.

--- AWAX/assets/js/script.js
+++ AWAX/assets/js/script.js
@@ -1,12 +1,12 @@
-function infoToggle(e) {
-    let div = document.querySelector(e);
+// function infoToggle(e) {
+//     let div = document.querySelector(e);
 
-    if (div.classList.contains('showInfo')) {
-        div.classList.remove('showInfo');
-    } else {
-        div.classList.add('showInfo');
-    };
-};
+//     if (div.classList.contains('showInfo')) {
+//         div.classList.remove('showInfo');
+//     } else {
+//         div.classList.add('showInfo');
+//     };
+// };
 
 function scrollToggle() {
     const element = document.querySelector('#scrollTopButton');
@@ -57,29 +57,41 @@ function activeSlide(area, slide) {
 
 function changeSlide(area, slide) {
     let sliders = document.querySelector(area + ' .sliders');
+    let screenWidth = window.screen.width;
 
     switch(slide) {
         case 0: sliders.style = 'margin-left: 0';
                 break;
 
         case 1: 
-                if (area == '.section-team-area') {
-                    // TO DO: Somar o tamanho dos cards dos slides. Os 280 + a margem de 15 = 295. 
-                    // Depois multiplcar esse valor pela quantidade de cards que aparecem na tela, 
-                    // , pode ser no máximo 3 e diminui para 2 e 1, coforme o tamanho da tela.
+                if (area == '.section-team-area' && (screenWidth > 450 && screenWidth <= 800)) {                    
                     sliders.style = 'margin-left: -590px';
+                } else if (area == '.section-team-area' && screenWidth <= 450) {
+                    sliders.style = 'margin-left: -295px';
+                } else if (area == '.section-team-area') {
+                    sliders.style = 'margin-left: -885px';
                 } else {
                     sliders.style = 'margin-left: -100vw';
                 };                
                 break;
         
-        case 2:
-                if (area == '.section-team-area') {
+        case 2:                                        
+                // Somar o tamanho dos cards dos slides. Os 280 + a margem de 15 = 295. 
+                    // Depois multiplcar esse valor pela quantidade de cards que aparecem na tela, 
+                    // pode ser no máximo 3 e diminui para 2 e 1, coforme o tamanho da tela.
+                    // Conforme passa o slide, aumentar o tamanho da margem, o valor total do tamanho dos slides "295" 
+                    // multiplicado pela quantidade de cards que aparecem na tela, ex. 295 * 2 cards = 590. 
+                    // E então somar esse valor ao total de margem que já possui.
+                if (area == '.section-team-area' && (screenWidth > 450 && screenWidth <= 800)) {                    
                     sliders.style = 'margin-left: -1180px';
+                } else if (area == '.section-team-area' && screenWidth <= 450) {
+                    sliders.style = 'margin-left: -590px';
+                } else if (area == '.section-team-area') {
+                    sliders.style = 'margin-left: -1770px';
                 } else {
                     sliders.style = 'margin-left: -200vw';
                 };                
-                break;                
+                break;
     } 
     
     activeSlide(area, slide);
